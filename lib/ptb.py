d4c491b6c77c0dee97e8f4d83400f89782de3399
Max Lapan <max.lapan@gmail.com>
2016-11-20 17:53:05 +0100

    Works

--- lib/ptb.py
+++ lib/ptb.py
@@ -11,7 +11,8 @@ class PTBDataset:
         self.vocab = vocab
         vocab.build(self._read_train())
 
-        self.train_x, self.train_y = self._build_samples(self._read_train(), limit=10000)
+        self.train_x, self.train_y = self._build_samples(self._read_train())
+        self.progress = 0.0
 
     def __str__(self):
         return "PTBData[vocab=%s, train_samples=%d]" % (self.vocab, len(self.train_x))
@@ -31,17 +32,19 @@ class PTBDataset:
         for token in tokens_iter:
             token_id = self.vocab.encode(token)
 
-            # if we have complete data sample, append
-            if len(window) == self.num_steps:
-                res_x.append(window)
-                res_y.append(token_id)
-                if limit is not None and len(res_x) == limit:
-                    break
-
             # push new entry
+            prev_window = list(window)
             window.append(token_id)
             if len(window) > self.num_steps:
                 window = window[-self.num_steps:]
+
+            # if we have complete data sample, append
+            if len(prev_window) == self.num_steps:
+                res_x.append(np.array(prev_window))
+                res_y.append(np.array(window))
+                if limit is not None and len(res_x) == limit:
+                    break
+
             # cleanup window at the end of sentence
             if token == self.vocab.eos_token():
                 window = []
@@ -58,8 +61,10 @@ class PTBDataset:
             self.train_y = self.train_y[shuffle_idx]
 
         ofs = 0
+        self.progress = 0.0
         while ofs + batch_size <= len(self.train_x):
             x = self.train_x[ofs:ofs+batch_size]
             y = self.train_y[ofs:ofs+batch_size]
             yield x, y
             ofs += batch_size
+            self.progress = ofs / len(self.train_x)
--- rnn/ptb_rnn_train.py
+++ rnn/ptb_rnn_train.py
@@ -1,4 +1,6 @@
+import numpy as np
 import tensorflow as tf
+from tensorflow.python.ops.seq2seq import sequence_loss
 import logging as log
 
 import sys
@@ -10,6 +12,7 @@ BATCH = 64
 NUM_STEPS = 10
 EMBEDDING = 100
 CELL_SIZE = 100
+LR = 0.001
 
 if __name__ == "__main__":
     log.basicConfig(level=log.INFO, format="%(asctime)s %(levelname)s %(message)s")
@@ -20,18 +23,36 @@ if __name__ == "__main__":
 
     with tf.Session() as session:
         ph_input = tf.placeholder(tf.int32, shape=(None, NUM_STEPS), name="input")
+        ph_labels = tf.placeholder(tf.int32, shape=(None, NUM_STEPS), name="labels")
         cell = tf.nn.rnn_cell.BasicRNNCell(CELL_SIZE)
+        cell = tf.nn.rnn_cell.OutputProjectionWrapper(cell, data.vocab.size())
         initial_state = cell.zero_state(BATCH, dtype=tf.float32)
 
         with tf.variable_scope("Model"):
             embedding = tf.get_variable("embedding", [data.vocab.size(), EMBEDDING])
             inputs = tf.nn.embedding_lookup(embedding, ph_input)
             inputs = [tf.squeeze(val, squeeze_dims=[1]) for val in tf.split(split_dim=1, num_split=NUM_STEPS, value=inputs)]
-        print(inputs)
         outputs, state = tf.nn.rnn(cell, inputs, initial_state=initial_state)
-        print(outputs)
-        print(state)
 
-#        for train_x, train_y in data.iterate_train(BATCH):
-#            print(len(train_x), len(train_y))
+        targets = tf.split(split_dim=1, num_split=NUM_STEPS, value=ph_labels)
+        loss_t = sequence_loss(outputs, targets, [tf.constant(1.0, tf.float32) for _ in range(NUM_STEPS)])
+        opt = tf.train.AdamOptimizer(LR)
+        opt_t = opt.minimize(loss_t)
+
+        session.run(tf.initialize_all_variables())
+        losses = []
+
+        epoch = 0
+        while True:
+            for iter_no, (train_x, train_y) in enumerate(data.iterate_train(BATCH)):
+                loss, _ = session.run([loss_t, opt_t], feed_dict={
+                    ph_input: train_x,
+                    ph_labels: train_y
+                })
+                losses.append(loss)
+                if iter_no % 100 == 0:
+                    log.info("Epoch=%d, iter=%d, epoch_perc=%.2f%%, mean_loss=%s",
+                             epoch, iter_no, data.progress*100.0, np.array(losses).mean())
+                    losses = []
+            epoch += 1
     pass

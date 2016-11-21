ce85fde2d09de79793ae3a1a38e6c5aa91e40286
Max Lapan <max.lapan@gmail.com>
2016-11-21 06:39:01 +0100

    Summary writer

--- rnn/ptb_rnn_train.py
+++ rnn/ptb_rnn_train.py
@@ -1,3 +1,5 @@
+import os
+import argparse
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.ops.seq2seq import sequence_loss
@@ -14,8 +16,18 @@ EMBEDDING = 100
 CELL_SIZE = 100
 LR = 0.001
 
+LOG_DIR = "logs"
+
 if __name__ == "__main__":
     log.basicConfig(level=log.INFO, format="%(asctime)s %(levelname)s %(message)s")
+
+    if not os.path.exists(LOG_DIR):
+        os.makedirs(LOG_DIR)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--name", default="rnn", help="Name of the run used in saving logs and models")
+    parser.add_argument("--max-epoch", type=int, help="If specified, stop after given amount of epoches, default=no limit")
+    args = parser.parse_args()
 
     log.info("Loading PTB dataset...")
     data = ptb.PTBDataset("data", vocab.Vocab(), num_steps=10)
@@ -39,11 +51,18 @@ if __name__ == "__main__":
         opt = tf.train.AdamOptimizer(LR)
         opt_t = opt.minimize(loss_t)
 
+        # summaries
+        writer = tf.train.SummaryWriter(os.path.join(LOG_DIR, args.name), session.graph)
+        summary_loss_ph = tf.placeholder(tf.float32, name='loss')
+        tf.scalar_summary("loss", summary_loss_ph)
+
+        summaries = tf.merge_all_summaries()
         session.run(tf.initialize_all_variables())
         losses = []
 
+        global_step = 0
         epoch = 0
-        while True:
+        while args.max_epoch is None or args.max_epoch > epoch:
             for iter_no, (train_x, train_y) in enumerate(data.iterate_train(BATCH)):
                 loss, _ = session.run([loss_t, opt_t], feed_dict={
                     ph_input: train_x,
@@ -51,8 +70,15 @@ if __name__ == "__main__":
                 })
                 losses.append(loss)
                 if iter_no % 100 == 0:
+                    m_loss = np.mean(losses)
                     log.info("Epoch=%d, iter=%d, epoch_perc=%.2f%%, mean_loss=%s",
-                             epoch, iter_no, data.progress*100.0, np.array(losses).mean())
+                             epoch, iter_no, data.progress*100.0, m_loss)
+                    summ_res, = session.run([summaries], feed_dict={
+                        summary_loss_ph: m_loss,
+                    })
+                    writer.add_summary(summ_res, global_step)
+                    writer.flush()
                     losses = []
+                global_step += 1
             epoch += 1
     pass
